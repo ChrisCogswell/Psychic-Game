7117e83596eddb946391dd977912a45986eaf4b6
ChrisCogs <ccogs@yahoo.com>
2019-02-27 17:59:09 -0500

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+
+
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+
+var comChoice = [computerChoices.length];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var userGuess = document.getElementById("letters-guessed");
+var guessesLeft = document.getElementById("guesses-left");
+
+document.onkeyup = function(event) {
+    var userChoice = event.key;
+
+    var comSelect = comChoice[Math.floor(Math.random() * comChoice)];
+
+    if ((userChoice === comSelect)) {
+        wins++;
+
+    } else {
+        losses++;
+    }
+
+}
+
