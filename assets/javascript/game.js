13d2e27aab84bf2df615c674f9d3b545cc9d27f0
ChrisCogs <ccogs@yahoo.com>
2019-03-03 15:15:05 -0500

    Commit 6, Added style and guesses reset on loss and win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,42 @@
+    var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 9;
+    
+    var comChoiceText = document.getElementById("comchoice-text");
+    var userGuessText = document.getElementById("guessSoFar-text");
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+    var guessesText = document.getElementById("guesses-left");
+
+    
+    document.onkeyup = function(event) {
+
+    var guess = event.key;    
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+   
+ 
+        if (computerGuess === guess){
+            wins++;
+            guessesLeft = 9;
+        }else {
+            guessesLeft--;
+        }
+
+        if (guessesLeft === 0){
+            losses++;
+            guessesLeft = 9;
+        }
+       
+   
 
 
-var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-var comChoice = [computerChoices.length];
-
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var guessChoice =[];
-
-
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var userGuess = document.getElementById("letters-guessed");
-var guessesLeft = document.getElementById("guesses-left");
-
-document.onkeyup = function(event) {
-    var userChoice = event.key;
-
-    var comSelect = comChoice[Math.floor(Math.random() * comChoice)];
-
-    if ((userChoice === comSelect)) {
-        wins++;
-
-    } else {
-        losses++;
+        comChoiceText.textContent = "The computer chose: " + computerGuess;
+        winsText.textContent = "wins: " + wins;
+        lossesText.textContent = "losses: " + losses;
+        userGuessText.textContent = "Guesses so far: " + guess;
+        guessesText.textContent = "Guesses left: " + guessesLeft;
     }
-
-
-    userGuess.textContent = "You Guessed " + userChoice; 
-}
-
+    
