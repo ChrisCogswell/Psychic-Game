46ebf06d1fd0bff668557733fb80a5a6f63d9751
ChrisCogs <ccogs@yahoo.com>
2019-03-11 13:48:22 -0400

    Added win and lose text, minor adjustments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+    // Game Variables
+
     var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     var wins = 0;
     var losses = 0;
@@ -8,8 +10,10 @@
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
     var guessesText = document.getElementById("guesses-left");
-
+    var starterText = document.getElementById("game-start");
     
+    // This function describes the game loop parameters
+
     document.onkeyup = function(event) {
 
     var guess = event.key;    
@@ -21,18 +25,20 @@
         if (computerGuess === guess){
             wins++;
             guessesLeft = 9;
+            starterText.textContent = "You Won";
         }else {
             guessesLeft--;
+            starterText.textContent = "Try Again";
         }
 
         if (guessesLeft === 0){
             losses++;
             guessesLeft = 9;
+            starterText.textContent = "You Lost";
         }
        
-   
 
-
+        
         comChoiceText.textContent = "The computer chose: " + computerGuess;
         winsText.textContent = "wins: " + wins;
         lossesText.textContent = "losses: " + losses;
